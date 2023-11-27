9c4038b7425c17bfaef9433d41f9631b08f9a45a
Julia Korzekwa <julia0n3@gmail.com>
2023-11-27 17:18:14 +0100

    add github action and deploy configuration in pom.xml

---
+++ src/main/java/com/befree/b3authauthorizationserver/config/configuration/EnableB3auth.java
@@ -0,0 +1,14 @@
+package com.befree.b3authauthorizationserver.config.configuration;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import(B3authConfiguration.class)
+public @interface EnableB3auth {
+}
