74179461ccfa3f5beaa80686aa779aa0c2778a1a
Michal Chruscielski <michalch775@gmail.com>
2023-11-28 21:42:35 +0100

    endpoints list

--- src/main/java/com/befree/b3authauthorizationserver/config/configuration/B3authEndpointsList.java
+++ src/main/java/com/befree/b3authauthorizationserver/config/configuration/B3authEndpointsList.java
@@ -4,13 +4,13 @@ public final class B3authEndpointsList {
     // na poniedzialek
     public final static String USER_AUTHENTICATION_ATTEMPT = "/b3auth/attempt";
     // na poniedzialek
-    public final static String USER_AUTHENTICATION = "b3auth/authenticate";
-    public final static String USER_TOKEN_REFRESH = "b3auth/refresh";
-    public final static String USER_LOGOUT = "b3auth/logout";
-    public final static String TOKEN_AUTHORIZATION = "b3auth/token/authorize";
+    public final static String USER_AUTHENTICATION = "/b3auth/authenticate";
+    public final static String USER_TOKEN_REFRESH = "/b3auth/refresh";
+    public final static String USER_LOGOUT = "/b3auth/logout";
+    public final static String TOKEN_AUTHORIZATION = "/b3auth/token/authorize";
     // na poniedzialek
-    public final static String CLIENT_AUTHENTICATION = "b3auth/client/authenticate";
-    public final static String CLIENT_TOKEN_REFRESH = "b3auth/client/refresh";
-    public final static String CLIENT_LOGOUT = "b3auth/client/logout";
-    public final static String JWK = "b3auth/jwk";
+    public final static String CLIENT_AUTHENTICATION = "/b3auth/client/authenticate";
+    public final static String CLIENT_TOKEN_REFRESH = "/b3auth/client/refresh";
+    public final static String CLIENT_LOGOUT = "/b3auth/client/logout";
+    public final static String JWK = "/b3auth/jwk";
 }
